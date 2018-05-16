fc7581b2df1d9e61a5a5c83f076f17dcd2ccfd8d
Jimmy Thrasher <jimmy@jimmythrasher.com>
2018-05-16 10:13:22 -0400

    Get tests working again after recent refactor

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,25 +1,25 @@
 require 'minitest/autorun'
-require 'build_audulus_wavetable_node'
+require 'command'
 
 class AllTest < Minitest::Test
   def test_argument_parsing
-    options = parse_arguments!([])
+    options = Command.parse_arguments!([])
     assert(options.has_key?(:help))
 
-    options = parse_arguments!(["-s"])
+    options = Command.parse_arguments!(["-s"])
     assert(options.has_key?(:help))
 
-    options = parse_arguments!(["-h"])
+    options = Command.parse_arguments!(["-h"])
     assert(options.has_key?(:help))
 
-    options = parse_arguments!(["--help"])
+    options = Command.parse_arguments!(["--help"])
     assert(options.has_key?(:help))
 
-    options = parse_arguments!(["filename"])
+    options = Command.parse_arguments!(["filename"])
     assert_equal("filename", options[:input_filename])
     refute(options[:spline_only])
 
-    options = parse_arguments!(["-s", "filename"])
+    options = Command.parse_arguments!(["-s", "filename"])
     assert_equal("filename", options[:input_filename])
     assert(options[:spline_only])
   end
