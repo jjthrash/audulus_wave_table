759c522abd8ff76d2e03db19be955f3f4290d0f3
Jimmy Thrasher <jimmy@jimmythrasher.com>
2018-05-23 10:30:04 -0400

    Get tests passing again after moving stuff around

--- test/test_all.rb
+++ test/test_all.rb
@@ -6,9 +6,6 @@ class AllTest < Minitest::Test
     options = Command.parse_arguments!([])
     assert(options.has_key?(:help))
 
-    options = Command.parse_arguments!(["-s"])
-    assert(options.has_key?(:help))
-
     options = Command.parse_arguments!(["-h"])
     assert(options.has_key?(:help))
 
@@ -18,13 +15,5 @@ class AllTest < Minitest::Test
     options = Command.parse_arguments!(["filename"])
     assert_equal("filename", options[:input_filename])
     refute(options[:spline_only])
-
-    options = Command.parse_arguments!(["-s", "filename"])
-    assert_equal("filename", options[:input_filename])
-    assert(options[:spline_only])
-
-    options = Command.parse_arguments!(["-m", "filename"])
-    assert_equal("filename", options[:input_filename])
-    assert(options[:midi])
   end
 end
